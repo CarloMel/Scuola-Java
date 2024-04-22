5dacf9c1c6930ac783b7df998a70a4698970578b
CarloExtendsPersona <carlomelluso1995@gmail.com>
2024-04-22 18:34:04 +0200

    Tutto risolto

---
+++ src/ScuolaTest.java
@@ -0,0 +1,8 @@
+public class ScuolaTest {
+    
+    public static void main(String[] args) {
+        Scuola scuola = new Scuola();
+        scuola.aggiungiStudente();
+        scuola.stampaStudenti();
+    }
+}
