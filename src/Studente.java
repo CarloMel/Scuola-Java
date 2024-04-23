967442a107e49bbaea5ee20ebeb1faa57a118683
CarloExtendsPersona <carlomelluso1995@gmail.com>
2024-04-23 11:43:56 +0200

    Setter  Studente.anno

--- src/Studente.java
+++ src/Studente.java
@@ -6,6 +6,7 @@ public class Studente extends Persona {
     private String matricola;
 
     Scanner scannerNumero = new Scanner(System.in);
+    Scanner scannerTesto = new Scanner(System.in);
     
     @Override
     public void setAnnoDiNascita(int annoDiNascita) {
@@ -34,7 +35,17 @@ public class Studente extends Persona {
 
 
     public void setClasse(int classe) {
-        this.classe = classe;
+        boolean ripeti = true;
+        do {
+            if(classe >= 1 &&  classe <= 5) {
+                this.classe = classe;
+                ripeti = false;
+            } else {
+                System.out.println("Dato non corretto");
+                System.out.println("Le classi vanno da 1 a 5");
+                classe = scannerNumero.nextInt();
+            }
+        } while (ripeti);
     }
 
 
